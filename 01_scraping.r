0c08f5225aab06caf02ee67f528f843156271499
Steffen <steffen.stell@gmail.com>
2019-05-14 23:54:12 +0200

    make scraping fully replicable

--- 01_scraping.r
+++ 01_scraping.r
@@ -1,40 +1,42 @@
-library(tidyverse)
-library(tabulizer)
-
-# https://www.wahl-o-mat.de/europawahl2019/Positionsvergleich-Europawahl2019.pdf
-
 # Tesseract ---------------------------------------------------------------
 library(magick)
 library(tesseract)
-# tesseract_download("deu", getwd()) 
+library(tidyverse)
 
-files <- str_c("Positionsvergleich-Europawahl2019_", 1:10, ".png")
+url <- "https://www.wahl-o-mat.de/europawahl2019/Positionsvergleich-Europawahl2019.pdf"
+download.file(url, "positions.pdf", mode = "wb")
+tesseract_download("eng", getwd()) 
+image_read_pdf("positions.pdf", 1:10, 600) %>% 
+  image_crop(geometry = "1140x5880+3540+630") %>% 
+  image_append(stack = F) %>% 
+  image_write("positions.png", format = "png")
 
-files %>% 
-  map(image_read) %>% 
-  map(image_crop, geometry = "1140x5880+3540+630") %>% 
-  map2(str_c("cut/", files), image_write, format = "png")
-
-wahl <- str_c("cut/", files) %>% 
-  map(ocr_data, engine = tesseract(language = "deu", 
-                                   datapath = getwd(),
-                                   options = list(tessedit_char_whitelist = "X_V"),
-                                   cache = F))
-
-wahl1 <- wahl %>% 
-  map(~.[["word"]]) %>% 
-  .[-(9:10)] %>% 
-  map(~matrix(., ncol = 4, byrow = T)) %>% 
-  map(as_tibble) %>% 
-  bind_cols()
-
-write_rds(wahl1, "data_pp.1-8.rds")
-
-wahl_ta <- ocr_data("samu_stinkt.png", 
-                    tesseract(language = "deu", 
+positions <- "positions.png" %>% 
+  ocr_data(engine = tesseract(language = "eng",
                               datapath = getwd(),
-                              options = list(tessedit_char_whitelist = "X_V")))
+                              options = list(tessedit_char_whitelist = "X_V"),
+                              cache = F))
 
+key = tibble(x = c("xX", "Y", "-", "VY", "x", "â\200”", "Xx", "v", "â\200”-", "Vv"),
+             y = c("Ablehnung", "Zustimmung", "Neutral", "Zustimmung", "Ablehnung", 
+                   "Neutral", "Ablehnung", "Zustimmung", "Neutral", "Zustimmung"))
 
+parteien <- c("CDU/CSU", "SPD", "Grüne", "DIE LINKE", 
+              "AfD", "FDP", "FREIE WÄHLER", "Piratenpartei", 
+              "Tierschutzpartei", "NPD", "Familienpartei","ÖDP", 
+              "Die PARTEI", "Volksabstimmung", "Bayernpartei", "DKP", 
+              "MLPD", "SGP", "TIERSCHUTZ", "Tierschutzallianz", 
+              "Bündnis C", "BIG", "BGE", "DIE DIREKTEI",
+              "DiEM25", "III.Weg", "Die Grauen", "DIE RECHTE", 
+              "DIE VIOLETTEN", "LIEBE", "DIE FRAUEN", "Graue Panther", 
+              "LKR", "MENSCHLICHE WELT", "NL", "ÖkoLinX", 
+              "Die Humanisten", "Partei für die Tiere", "Gesundheitsforschung", "Volt")
 
-matrix(wahl[[1]][["word"]], ncol = 4, byrow = T) %>% View
+positions <- positions %>% 
+  left_join(key, by = c("word" = "x")) %>% 
+  pull(y) %>% 
+  matrix(nrow = 38, ncol = 40, byrow = T) %>% 
+  as_tibble %>% 
+  set_names(parteien)
+
+write_rds(positions, "positions.rds")
